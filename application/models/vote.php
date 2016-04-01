d669d8a033e247e1d050b9b77e4df35ac93551dc
Thomas Eggleston <thomas_eggleston@outlook.com>
2016-04-01 16:50:21 +1300

    Updated query for retrieving votes
    It now uses a left join instead of a nested select query.

--- application/models/vote.php
+++ application/models/vote.php
@@ -1,24 +1,24 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Vote extends CI_Model {
-    
+
     public function __construct() {
         $this->load->database();
     }
-    
+
     /*
      * Returns a list of votes for each answer in the given poll.
      */
     public function getVotes($pollID) {
-        $sql = 'SELECT (SELECT count(*) '
-                .      'FROM Votes '
-                .      'WHERE Votes.PollID = Answers.PollID AND '
-                .      'Votes.AnswerNumber = Answers.AnswerNumber) AS votes '
-                . 'FROM Answers '
-                . 'WHERE Answers.PollID = ' . $pollID
-                .' ORDER BY Answers.id ASC';
+        $sql = "SELECT count(Votes.id) as votes
+                FROM Answers
+                LEFT JOIN Votes ON Answers.AnswerNumber = Votes.AnswerNumber
+                    AND Votes.PollID = {$pollID}
+                WHERE Answers.PollID = {$pollID}
+                GROUP BY Answer
+                ORDER BY Answers.id ASC";
         $rows = $this->db->query($sql)->result();
-        
+
         $list = array();
         foreach($rows as $row) {
             $list[] = array('votes' => $row->votes);
@@ -34,10 +34,10 @@ class Vote extends CI_Model {
         $voteData = array('AnswerNumber'=> $answerNumber,
                           'IpAddress'   => $ipAddress,
                           'PollID'      => $pollID);
-        
+
         $this->db->insert('Votes', $voteData);
     }
-    
+
     /*
      * Deletes all the existing rows in the Vote table which correspond to
      * the given poll.
